47ce3092791e0fcc287af89e4349db393ebadc96
Lecomte Matthieu <lecomte@b11p10.fil.univ-lille1.fr>
2014-11-24 12:08:42 +0100

    simpleArticle.html

--- src/main/java/blog/mvc/HomeController.java
+++ src/main/java/blog/mvc/HomeController.java
@@ -14,4 +14,8 @@ public class HomeController {
         return "view/home";
     }
 
+    @RequestMapping("/simpleArticle")
+    public String simpelArticle() {
+        return "view/simpleArticle";
+    }
 }
